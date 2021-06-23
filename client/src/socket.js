3ebdf58210c3a0db0f3ba0baa554a027971fe154
vkvamsi24 <59496691+vkvamsi24@users.noreply.github.com>
2021-06-23 18:20:30 +0530

    Added git ignore

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-// const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
-const sockets = io('/');
+ const sockets = io('https://serverteams.herokuapp.com/', { autoConnect: true, forceNew: true });
+// const sockets = io('/');
 export default sockets;
